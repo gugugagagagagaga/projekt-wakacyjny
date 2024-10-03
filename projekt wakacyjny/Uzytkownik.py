41e410adef4e2233192ade7ba03214047be4a8f2
gugugagagagagaga <146242047+gugugagagagagaga@users.noreply.github.com>
2024-10-03 21:35:20 +0200

    Uzytkownik.py

--- projekt wakacyjny/Uzytkownik.py
+++ projekt wakacyjny/Uzytkownik.py
@@ -2,7 +2,6 @@ class Uzytkownik():
     def __init__(self, nazwa, haslo):
         self.nazwa = nazwa
         self.haslo = haslo
-        #...
 
     def podaj_nazwe(self):
         return self.nazwa
